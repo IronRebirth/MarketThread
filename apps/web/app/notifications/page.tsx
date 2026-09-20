edbce2c6f462ba57dbf94e38048b454f57126e36
Tazbid H. <tazbidhasan.connect@gmail.com>
2026-09-20 16:22:47 +0600

    feat: add notification inbox UI

---
+++ apps/web/app/notifications/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "../../components/layout/app-shell";
+import { NotificationInbox } from "../../components/notifications/notification-inbox";
+
+export default function NotificationsPage() {
+  return (
+    <AppShell>
+      <NotificationInbox />
+    </AppShell>
+  );
+}
